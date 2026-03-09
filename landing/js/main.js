609a461698afbdc69b6c238354a7d2057648c9d4
NRED <nred@NREDui-iMac.local>
2026-03-09 10:39:23 +0900

    fix: improve mobile viewport and view layout responsiveness

--- landing/js/main.js
+++ landing/js/main.js
@@ -31,33 +31,33 @@ window.addEventListener("mouseleave", () => {
 	}
 })
 
-cards.forEach(card=>{
+cards.forEach(card => {
 
-card.addEventListener("mousemove",(e)=>{
+	card.addEventListener("mousemove", (e) => {
 
-const rect = card.getBoundingClientRect()
+		const rect = card.getBoundingClientRect()
 
-const x = e.clientX - rect.left
-const y = e.clientY - rect.top
+		const x = e.clientX - rect.left
+		const y = e.clientY - rect.top
 
-card.style.setProperty("--x",x+"px")
-card.style.setProperty("--y",y+"px")
+		card.style.setProperty("--x", x + "px")
+		card.style.setProperty("--y", y + "px")
 
-const centerX = rect.width/2
-const centerY = rect.height/2
+		const centerX = rect.width / 2
+		const centerY = rect.height / 2
 
-const rotateX = (y-centerY)/18
-const rotateY = (centerX-x)/18
+		const rotateX = (y - centerY) / 18
+		const rotateY = (centerX - x) / 18
 
-card.style.transform =
-`rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`
+		card.style.transform =
+			`rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`
 
-})
+	})
 
-card.addEventListener("mouseleave",()=>{
+	card.addEventListener("mouseleave", () => {
 
-card.style.transform=""
+		card.style.transform = ""
 
-})
+	})
 
 })
